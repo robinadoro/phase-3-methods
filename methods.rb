5108001ae5e0770d958671d496e17f19a41f5748
Robinson Idagiza <robinadoro@gmail.com>
2022-06-07 06:47:39 +0300

    ft:completed

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+
+end
+
+def greet (name=robin)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+  end
+
+  def add num1, num2
+    return num1 + num2
+  end
+
+  def halve (numb)    
+    if numb.class != Integer
+        return nil
+       
+    end
+    numb/2
+end
+
+
+
